2de3399895272ec41208cdf872e3d72c2716cf56
Kay Callahan and Troy Oram <marissa.callahan@gmail.com+oram0720@gmail.com>
2018-11-06 11:38:18 -0500

    Add auth forms to client

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,32 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events.js')
+const config = require('./config.js')
 
 $(() => {
   // your JS code goes here
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out-form').on('click', authEvents.onSignOut)
+
+  $('#upload-form').on('submit', function (event) {
+    event.preventDefault()
+    const data = new FormData(event.target)
+    $.ajax({
+      method: 'POST',
+      url: config.apiUrl + '/uploads',
+      processData: false, // Important!
+      contentType: false,
+      data: data
+    })
+      .then((response) => {
+        console.log(response)
+        $('#new-image').attr('src', response.upload.url)
+      })
+      .catch((response) => {
+        console.log(response)
+      })
+  })
 })
