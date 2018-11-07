d9bd191b9e85a4c9e5909454a8109b5a5149e4ea
maggile <marissa.callahan@gmail.com>
2018-11-06 22:51:33 -0500

    added read me template

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -29,7 +29,6 @@ const onChangePassword = function (event) {
 }
 
 const onSignOut = function (event) {
-  event.preventDefault()
   api.signOut()
     .then(ui.signOutSuccess)
     .catch(ui.signOutFailure)
